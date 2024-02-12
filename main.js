3e5dd59bbf917c5b715a3562d23c19f2b59aca43
MaluMSiza <93840950+MaluMSiza@users.noreply.github.com>
2024-02-12 12:58:18 -0300

    electron rodando

---
+++ main.js
@@ -0,0 +1,48 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const expressApp = express();
+const PORT = process.env.PORT || 3000;
+
+expressApp.use(express.static('public'));
+expressApp.use(bodyParser.json());
+
+expressApp.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+// Adicione outras rotas aqui conforme necessário
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true // Habilita a integração com Node.js no contexto da página web
+    }
+  });
+
+  mainWindow.loadURL('http://localhost:' + PORT);
+};
+
+app.whenReady().then(() => {
+  expressApp.listen(PORT, () => {
+    console.log(`Express server running on http://localhost:${PORT}`);
+  });
+
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
