3101912a6c8d7b62867ada4c515c71621f655773
Gaarcos <gabrielsuehiro@gmail.com>
2024-03-12 18:58:07 -0300

    mudança de cpf pra username

--- backend/banco/conexao.py
+++ backend/banco/conexao.py
@@ -3,5 +3,5 @@ import pymongo
 def conectar_banco():
     client = pymongo.MongoClient("mongodb://localhost:27017/")
     db = client["BioKinectCare"]
-    collection = db["pacientes"]
+    collection = db["pacientes2.0"]
     return collection
